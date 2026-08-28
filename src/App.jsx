8b39d6bb11f55275a9993371e7555d5806b406bb
Painel Central de prospecção (27) <evertondireito13@gmail.com>
2026-08-27 23:21:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import TrustBar from './components/TrustBar.jsx'
-import QuemSomos from './components/QuemSomos.jsx'
 import Method from './components/Method.jsx'
-import MethodDeep from './components/MethodDeep.jsx'
-import Atuacao from './components/Atuacao.jsx'
+import Team from './components/Team.jsx'
 import Services from './components/Services.jsx'
 import HowItWorks from './components/HowItWorks.jsx'
 import ClientPanel from './components/ClientPanel.jsx'
-import Tecnologia from './components/Tecnologia.jsx'
 import Testimonials from './components/Testimonials.jsx'
 import FAQ from './components/FAQ.jsx'
-import Contato from './components/Contato.jsx'
 import LeadForm from './components/LeadForm.jsx'
 import Footer from './components/Footer.jsx'
-import WhatsAppButton from './components/WhatsAppButton.jsx'
 
 export default function App() {
   return (
@@ -23,22 +18,16 @@ export default function App() {
       <main>
         <Hero />
         <TrustBar />
-        <QuemSomos />
         <Method />
-        <MethodDeep />
+        <Team />
         <Services />
-        <Atuacao />
         <HowItWorks />
         <ClientPanel />
-        <Tecnologia />
         <Testimonials />
         <FAQ />
-        <Contato />
         <LeadForm />
       </main>
       <Footer />
-      <WhatsAppButton />
     </div>
   )
 }
-
